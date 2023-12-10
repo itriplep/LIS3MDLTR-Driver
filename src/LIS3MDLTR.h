8182d8f68c2dfc7768363e730493a6ef6e9272e1
TripleP <itriplep@github.com>
2023-12-10 15:00:34 +0000

    UPDATE LIS3MDLTR.h          and add the prototype of function and stuct of sensor

--- src/LIS3MDLTR.h
+++ src/LIS3MDLTR.h
@@ -22,6 +22,9 @@
 
 // Compiler Includes
 // All include files that are provided by the compiler directly
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 //Project Includes
 //All include files that are provided by the project
@@ -36,11 +39,39 @@
 // Type definitions
 //--------------------------------------------------------------------------------------------------------
 
+//structure pf LIS3MDLTR sensor
+struct LIS3MDLTR{
+  bool initialization;
+  uint8_t device_id; //buss_address of sensor
+};
+
+//Function status for error handeling
+typedef enum FunctionStatus{
+  FUNCTION_STATUS_OK = 0,
+  FUNCTION_STATUS_ERROR
+}FunctionStatus;
 
 //--------------------------------------------------------------------------------------------------------
 // Functions declartions
 //--------------------------------------------------------------------------------------------------------
 
+//Constructor for sensor structure
+FunctionStatus LIS3MDLTR_Constructor(struct LIS3MDLTR *self_ptr, uint8_t device_id);
+
+//Retrieve the device’s full-scale configuration
+FunctionStatus LIS3MDLTR_GetFullScaleConfig(struct LIS3MDLTR *self_ptr, uint8_t* fullscale);
+
+//Set the device’s output data rate
+FunctionStatus LIS3MDLTR_ChangeOutputDataRate(struct LIS3MDLTR *self_ptr, uint8_t rate);
+
+//Retrieve the device’s output data rate
+FunctionStatus LIS3MDLTR_GetOutputDataRate(struct LIS3MDLTR *self_ptr, uint8_t* datarate);
+
+//Enable or disable the device’s interrupt pin
+FunctionStatus LIS3MDLTR_ChangeInteruptPinStatus(struct LIS3MDLTR *self_ptr, uint8_t interupt_status);
+
+//Read the output data of a specific axis
+FunctionStatus LIS3MDLTR_ReadAxis(struct LIS3MDLTR *self_ptr, uint8_t axis_to_read, uint16_t* data_ptr);
 
 //--------------------------------------------------------------------------------------------------------
 // Function definitions
